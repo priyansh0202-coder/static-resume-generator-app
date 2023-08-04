49afd9c3183a3cc4e1e40c9791d12fb4cc60d44c
priyansh0202-coder <priyanshsoni36@gmail.com>
2023-08-04 08:31:02 +0530

    initial

--- script.js
+++ script.js
@@ -25,5 +25,64 @@ function addNewAQfield() {
     aqOb.insertBefore(newNode, aqAddbuttonOb);
 }
 
+//generatingcv//
+function generateCV() {
+
+    let namefield = document.getElementById("namefield").value;
+    let nameT1 = document.getElementById("nameT1");
+    nameT1.innerHTML = namefield;
+
+    //direct
+    document.getElementById("nameT2").innerHTML = namefield;
+
+    //contact
+
+    document.getElementById("contactT").innerHTML = document.getElementById("contactField").value;
+
+    //address
+
+    document.getElementById("addressT").innerHTML = document.getElementById("AddressField").value;
+    document.getElementById("fbT").innerHTML = document.getElementById("fbfield").value;
+    document.getElementById("instaT").innerHTML = document.getElementById("instafield").value;
+    document.getElementById("linkdinT").innerHTML = document.getElementById("linkdinfield").value;
+
+
+    //objective
+
+    document.getElementById("objectiveT").innerHTML = document.getElementById("objectiveField").value;
+
+    //    work exp
+    let wes = document.getElementsByClassName("wefield");
+
+    let str = "";
+
+    for (let e of wes) {
+        str = str + `<li> ${e.value} </li>`;
+    }
+
+    document.getElementById("weT").innerHTML = str;
+
+    //  academic
+
+    let aqs = document.getElementsByClassName("eqfield");
+
+    let str1 = "";
+
+    for (let e of aqs) {
+        str1 += `<li> ${e.value} </li>`;
+    }
+
+    document.getElementById("aqT").innerHTML = str1;
+
+    document.getElementById("cv-form").style.display = "none";
+    document.getElementById("cv-template").style.display = "block"
+
+}
+
+//printcv
+
+function printCV() {
+    window.print();
+}
 
 
